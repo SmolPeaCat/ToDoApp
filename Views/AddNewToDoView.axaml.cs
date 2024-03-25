78ec1b79b6c5a41294b550eaa88a66d39f8ddfd8
smolpeacat <smolpeacat@gmail.com>
2024-03-25 21:28:14 +0100

    Finished my mockup
    Added a bunch of stuff like
    - the mockup of my app
    - The beatiful background
    - The other views

---
+++ Views/AddNewToDoView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimpleTodoList.Views;
+
+public partial class AddNewToDoView : UserControl
+{
+    public AddNewToDoView()
+    {
+        InitializeComponent();
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace SimpleTodoList.Views;
 
@@ -7,5 +9,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MakeItBeautiful();
+    }
+
+    private void MakeItBeautiful()
+    {
+        var gradientBrush = new LinearGradientBrush();
+        gradientBrush.GradientStops.Add(new GradientStop(Colors.Red, 0.0));
+        gradientBrush.GradientStops.Add(new GradientStop(Colors.Blue, 1.0));
+        BackOfToDos.Background = gradientBrush;
     }
 }
